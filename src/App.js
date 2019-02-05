37486f9440f41ae554612382ee6c181cbbc5c65c
James Almond <jamestalmond@gmail.com>
2019-02-05 15:56:18 +0000

    Intercepting route changes

--- src/App.js
+++ src/App.js
@@ -1,47 +1,28 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import {
-	BrowserRouter as Router,
-	Route,
-	Link,
-	Redirect
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Prompt } from 'react-router-dom';
 
-const loggedin = true;
-const Links = () => (
-	<nav>
-		<Link to="/">Home</Link>
-		<Link to="/old/123">Old</Link>
-		<Link to="/new/456">New</Link>
-		<Link to="/protected">Protected</Link>
-	</nav>
-);
-
+const Home = () => <h1>Home</h1>;
+class Form extends React.Component {
+	state = { dirty: false };
+	setDirty = () => this.setState({ dirty: true });
+	render() {
+		return (
+			<div>
+				<h1>Form</h1>
+				<input type="text" onInput={this.setDirty} />
+				<Prompt when={this.state.dirty} message="Data will be lost!" />
+			</div>
+		);
+	}
+}
 const App = props => (
 	<Router basename={props.path}>
 		<div>
-			<Links />
-			<Route exact path="/" render={() => <h1>Home</h1>} />
-			<Route
-				path="/new/:str"
-				render={({ match }) => <h1>New: {match.params.str}</h1>}
-			/>
-
-			<Route
-				path="/old/:str"
-				render={({ match }) => <Redirect to={`/new/${match.params.str}`} />}
-			/>
-
-			<Route
-				path="/protected"
-				render={() =>
-					loggedin ? (
-						<h1>Welcome to the protected page</h1>
-					) : (
-						<Redirect to="/new/Login" />
-					)
-				}
-			/>
+			<Link to="/">Home</Link>
+			<Link to="/form">Form</Link>
+			<Route exact path="/" component={Home} />
+			<Route path="/form" component={Form} />
 		</div>
 	</Router>
 );
