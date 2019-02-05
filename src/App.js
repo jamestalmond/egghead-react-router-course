0c237769565d9452d8fbbe7e769f4568d7818164
James Almond <jamestalmond@gmail.com>
2019-02-05 15:06:32 +0000

    Conditionally render a route with switch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import ReactDOM from 'react-dom';
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import './App.css';
 
 const Links = () => (
 	<nav>
 		<Link to="/">Home</Link>
 		<Link to="/about">About</Link>
-		<Link to="/contact/xxx/xxx/xxxx/x">Contact</Link>
+		<Link to="/contact">Contact</Link>
+		<Link to="/123456">Item ID 123456</Link>
 	</nav>
 );
 
@@ -18,7 +20,11 @@ const App = props => (
 			<Switch>
 				<Route exact path="/" render={() => <h1>Home</h1>} />
 				<Route path="/about" render={() => <h1>About</h1>} />
-				<Route render={() => <h1>No idea mate</h1>} />
+				<Route path="/contact" render={() => <h1>Contact</h1>} />
+				<Route
+					path="/:itemid"
+					render={({ match }) => <h1>Item: {match.params.itemid}</h1>}
+				/>
 			</Switch>
 		</div>
 	</Router>
